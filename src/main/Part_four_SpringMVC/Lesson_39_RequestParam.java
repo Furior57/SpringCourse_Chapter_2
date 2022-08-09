4ceb3da3422df94938199bd972b72beb77bdac8a
Furior57 <Furior572@gmail.com>
2022-08-09 09:40:45 +0300

    09.08.22 RequestParam

---
+++ src/main/Part_four_SpringMVC/Lesson_39_RequestParam.java
@@ -0,0 +1,5 @@
+package main.Part_four_SpringMVC;
+
+public class Lesson_39_RequestParam {
+    // Это турок будет коротким. Перейдем в класс MyFirstController на 59 строку.
+}
--- src/main/Part_four_SpringMVC/service/controllers/MyFirstController.java
+++ src/main/Part_four_SpringMVC/service/controllers/MyFirstController.java
@@ -3,6 +3,7 @@ package main.Part_four_SpringMVC.service.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -37,23 +38,34 @@ public class MyFirstController {
     // Мы закомментировали метод showEmployeeDetails. Напишем новый.
     // Наша цель изменить то имя которое выводилось в сообщении. Как мы помним оно сохранено
     // в переменной в jsp файле.
-    @RequestMapping("/showDetails")
+//    @RequestMapping("/showDetails")
     // Прежде всего нам необходимо передать два параметра, первым идет интерфейс
     // HttpServletRequest, он отвечает за взаимодействие между сервером и клиентом и
     // облегчает получение данных. Нам он нужен, чтобы получить имя.
     // Второй параметр - интерфейс Model
-    public String showEmployeeDetails(HttpServletRequest request, Model model) {
-        // Получаем имя с помощью метода getParameter(), куда передаем строку с
-        // названием переменной где хранится наше имя.
-        String empName = request.getParameter("employeeName");
-        // Меняем имя
-        empName = "Mr. "+ empName;
-        // Добавляем в наш контейнер атрибут, первый параметр - имя атрибута, второй - значение.
-        // Чтобы использовать эти данные, в jsp файле, там где мы указывали имя параметра,
-        // мы просто укажем имя атрибута, который сюда записали, выглядеть это будет так:
-        // Your name: ${nameAttribute}, спринг сам подскажет все имена параметров.
+//    public String showEmployeeDetails(HttpServletRequest request, Model model) {
+//        // Получаем имя с помощью метода getParameter(), куда передаем строку с
+//        // названием переменной где хранится наше имя.
+//        String empName = request.getParameter("employeeName");
+//        // Меняем имя
+//        empName = "Mr. "+ empName;
+//        // Добавляем в наш контейнер атрибут, первый параметр - имя атрибута, второй - значение.
+//        // Чтобы использовать эти данные, в jsp файле, там где мы указывали имя параметра,
+//        // мы просто укажем имя атрибута, который сюда записали, выглядеть это будет так:
+//        // Your name: ${nameAttribute}, спринг сам подскажет все имена параметров.
+//        model.addAttribute("nameAttribute", empName);
+//
+//        return "show-emp-details-view";
+//    }
+    // Есть и другой способ передавать параметры из jsp файла, для этого используется
+    // аннотация @RequestParam, куда мы можем передать имя параметра. Эту аннотацию мы передаем
+    // параметром в метод, в скобки аннотации передаем строку с названием переменной
+    //  которую передаем, после указываем тип данных и название переменной уже в нашем методе.
+        @RequestMapping("/showDetails")
+    public String showEmployeeDetails(@RequestParam("employeeName") String empName,
+                                      Model model) {
+        empName = "Mr. "+empName;
         model.addAttribute("nameAttribute", empName);
-
         return "show-emp-details-view";
     }
 
